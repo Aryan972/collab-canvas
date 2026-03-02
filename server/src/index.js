a85c89168fc4b1267c138d42adefbdbff9109fa1
Aryan972 <32037736+Aryan972@users.noreply.github.com>
2026-03-02 20:55:47 +0530

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -12,7 +12,7 @@ const rooms = {};
 
 const io = new Server(server, {
   cors: {
-    origin: "**", // Vite frontend port
+    origin: "*", // Vite frontend port
     methods: ["GET", "POST"],
   },
 });
